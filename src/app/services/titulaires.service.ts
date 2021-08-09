096c9c56e6fbadf388321ad3ab76ad5791c28825
medMelehi <mohammadmelehi@gmail.com>
2021-08-09 20:44:46 +0100

    finish search

--- src/app/services/titulaires.service.ts
+++ src/app/services/titulaires.service.ts
@@ -1,6 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+export interface resp {
+  _embedded: any;
+  _links: any;
+  page: any;
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -11,6 +18,12 @@ export class TitulairesService {
   constructor(private httpClient:HttpClient) {   }
 
   public  getTitulaires(page:number , size:number){
-    return this.httpClient.get(this.host+"/titulaires?page="+page+"&size="+size);  
+    return this.httpClient.get<resp>(this.host+"/titulaires?page="+page+"&size="+size).pipe(
+      retry(3) // then handle the error
+    );  
+   }
+   public  searchTitulaires(matricule:String,nom:String,email:String,page:number , size:number){
+     console.log(this.host+"/titulaires/search/byNomEmailMatPage?nom="+nom+"&mat="+matricule+"&email="+email+"&page="+page+"&size="+size);
+    return this.httpClient.get<resp>(this.host+"/titulaires/search/byNomEmailMatPage?nom="+nom+"&mat="+matricule+"&email="+email+"&page="+page+"&size="+size);  
    }
 }
--- src/app/titulaires/titulaires.component.ts
+++ src/app/titulaires/titulaires.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { TitulairesService } from '../services/titulaires.service';
-
+import * as $ from "jquery";
 @Component({
   selector: 'app-titulaires',
   templateUrl: './titulaires.component.html',
@@ -11,9 +11,9 @@ import { TitulairesService } from '../services/titulaires.service';
 export class TitulairesComponent implements OnInit {
 
  public titulaires :any;
- public matricule:any;
- public nom:any;
- public email:any;
+ public matricule:String="";
+ public nom:String="";
+ public email:String="";
   public size:number=1;
   public currentPage:number=0;
   public totalPages:number;
@@ -22,28 +22,76 @@ export class TitulairesComponent implements OnInit {
   constructor( private titService:TitulairesService) { }
 
   ngOnInit(): void {
+    var self = this;
+    this.onSearchTitulaires();
+ 
+        $('#mat').focus(function(){
+          self.nom="";
+          self.email="";
+        });
+        $("#nom").focus(function(){
+          self.matricule="";
+          self.email="";
+        });
+        $("#email").focus(function(){
+          self.nom="";
+          self.matricule="";
+        });
+        $('input').keyup(function(event){
+
+          var mat = $('#mat').val();
+          var nom = $('#mat').val();
+          var email = $('#mat').val();
+          console.log("mat:"+mat+"-nom:"+nom+"-email:"+email);
+          if (mat =="" && nom =="" && email ==""  ) {
+            console.log("empty inputs");
+            self.onSearchTitulaires();
+            }
+          var keycode = (event.keyCode ? event.keyCode : event.which);
+          if(keycode+"" == '13'){
+            $('#search').click();
+          }
+      });
+        
+    
   }
 
-  onGetTitulaires(){
+ 
+  onPgaeTitulaire(i:number){
+    this.currentPage=i;
+    this.onSearchTitulaires();
+  }
+  onNextPgaeTitulaire(){
+    this.currentPage++;
+    this.onSearchTitulaires();
+  }
+  onPreviousPgaeTitulaire(){
+    this.currentPage--;
+    this.onSearchTitulaires();
+  }
+  onSearchTitulaires(){
+    console.log("search clicked");
+    this.titService.searchTitulaires(this.matricule,this.nom,this.email,this.currentPage,this.size)
+   .subscribe((data : any)=>{
+    this.titulaires = data;
+    console.log(data);
+    this.totalPages = data.page.totalPages;
+    this.pages=new Array<number>(this.totalPages);
+        },err=>{
+      console.log("errroooor"+err);
+    }) 
+  }
+
+
+  /*  onGetTitulaires(){
     this.titService.getTitulaires(this.currentPage,this.size)
    .subscribe(data=>{
       this.titulaires = data;
+      console.log(data);
       this.totalPages = (data as any).page.totalPages;
       this.pages=new Array<number>(this.totalPages);
         },err=>{
       console.log(err);
     }) 
-  }
-  onPgaeTitulaire(i:number){
-    this.currentPage=i;
-    this.onGetTitulaires();
-  }
-  onNextPgaeTitulaire(){
-    this.currentPage++;
-    this.onGetTitulaires();
-  }
-  onPreviousPgaeTitulaire(){
-    this.currentPage--;
-    this.onGetTitulaires();
-  }
+  } */
 }
